fd97c3e23f2cfe7515f2ec9921229a582d9630bb
Darren Jeacocke <dazonic@me.com>
2014-03-09 17:04:59 +1000

    Test event country

--- test/event_test.rb
+++ test/event_test.rb
@@ -8,15 +8,22 @@ class EventTest < MiniTest::Test
     @event.results << F1Results::RaceResult.new([2, 5, 'Fernando Alonso', 'Ferrari', 52, '+3.0 secs', 1, 18])
   end
 
+  def test_set_country
+    @event.country = 'Australia'
+    assert_equal 'Australia', @event.country
+    @event.country = 'great britain'
+    assert_equal 'Great Britain', @event.country
+  end
+
+  def test_set_results
+    event = F1Results::Event.new(:results => @event.results)
+    assert_equal @event.results, event.results
+  end
+
   def test_to_array
     array = @event.to_a
     assert_equal 2, array.length
     assert_kind_of Array, array.first
     assert_kind_of F1Results::RaceResult, @event.results.first
-  end
-
-  def set_results
-    event = F1Results::Event.new(:results => @event.results)
-    assert_equal @event.results, event.results
   end
 end
