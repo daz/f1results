57ac677fe15f771d7d6ae9ebeb2b24afb349fee1
Darren Jeacocke <dazonic@me.com>
2016-07-24 15:21:34 +1000

    Rename fixtures

--- test/lib/test_helper.rb
+++ test/lib/test_helper.rb
@@ -15,33 +15,33 @@ module Fixtures
 
     def events(name)
       @agent ||= F1Results::Agent.new
-      file = "#{name.to_s.gsub('_', '-')}.html"
+      file = "#{name.to_s}.html"
       @agent.get_results_with_url fixture_url(file)
     end
 
     def override_requests
       stub 'results.html/1900/races.html'                                  => 404
 
-      stub 'results.html/1950/races/96/indianapolis-500/fastest-laps.html' => '1950-indianapolis-500-fastest-laps.html'
-      stub 'results.html/1950/races/96/indianapolis-500/qualifying-0.html' => '1950-indianapolis-500-qualifying-0.html'
-      stub 'results.html/1950/races/96/indianapolis-500/race-result.html'  => '1950-indianapolis-500-race-result.html'
-      stub 'results.html/1950/races.html'                                  => '1950-races.html'
+      stub 'results.html/1950/races/96/indianapolis-500/fastest-laps.html' => '1950_indianapolis_500_fastest_laps.html'
+      stub 'results.html/1950/races/96/indianapolis-500/qualifying-0.html' => '1950_indianapolis_500_qualifying_0.html'
+      stub 'results.html/1950/races/96/indianapolis-500/race-result.html'  => '1950_indianapolis_500_race_result.html'
+      stub 'results.html/1950/races.html'                                  => '1950_races.html'
 
-      stub 'results.html/1984/brazil/466/fastest-laps.html'                => '1984-brazil-fastest-laps.html'
-      stub 'results.html/1984/brazil/466/qualifying-0.html'                => '1984-brazil-qualifying-0.html'
-      stub 'results.html/1984/brazil/466/qualifying-1.html'                => '1984-brazil-qualifying-1.html'
-      stub 'results.html/1984/brazil/466/qualifying-2.html'                => '1984-brazil-qualifying-2.html'
-      stub 'results.html/1984/brazil/466/race-result.html'                 => '1984-brazil-race-result.html'
-      stub 'results.html/1984/brazil/466/starting-grid.html'               => '1984-brazil-starting-grid.html'
-      stub 'results.html/1984/brazil/466/warm-up.html'                     => '1984-brazil-warm-up.html'
-      stub 'results.html/1984/races.html'                                  => '1984-races.html'
+      stub 'results.html/1984/brazil/466/fastest-laps.html'                => '1984_brazil_fastest_laps.html'
+      stub 'results.html/1984/brazil/466/qualifying-0.html'                => '1984_brazil_qualifying_0.html'
+      stub 'results.html/1984/brazil/466/qualifying-1.html'                => '1984_brazil_qualifying_1.html'
+      stub 'results.html/1984/brazil/466/qualifying-2.html'                => '1984_brazil_qualifying_2.html'
+      stub 'results.html/1984/brazil/466/race-result.html'                 => '1984_brazil_race_result.html'
+      stub 'results.html/1984/brazil/466/starting-grid.html'               => '1984_brazil_starting_grid.html'
+      stub 'results.html/1984/brazil/466/warm-up.html'                     => '1984_brazil_warm_up.html'
+      stub 'results.html/1984/races.html'                                  => '1984_races.html'
 
-      stub 'results.html/2016/races/938/australia/fastest-laps.html'       => '2016-australia-fastest-laps.html'
-      stub 'results.html/2016/races/938/australia/pit-stop-summary.html'   => '2016-australia-pit-stop-summary.html'
-      stub 'results.html/2016/races/938/australia/practice-1.html'         => '2016-australia-practice-1.html'
-      stub 'results.html/2016/races/938/australia/qualifying.html'         => '2016-australia-qualifying.html'
-      stub 'results.html/2016/races/938/australia/race-result.html'        => '2016-australia-race-result.html'
-      stub 'results.html/2016/races.html'                                  => '2016-races.html'
+      stub 'results.html/2016/races/938/australia/fastest-laps.html'       => '2016_australia_fastest_laps.html'
+      stub 'results.html/2016/races/938/australia/pit-stop-summary.html'   => '2016_australia_pit_stop_summary.html'
+      stub 'results.html/2016/races/938/australia/practice-1.html'         => '2016_australia_practice_1.html'
+      stub 'results.html/2016/races/938/australia/qualifying.html'         => '2016_australia_qualifying.html'
+      stub 'results.html/2016/races/938/australia/race-result.html'        => '2016_australia_race_result.html'
+      stub 'results.html/2016/races.html'                                  => '2016_races.html'
     end
 
     def stub(hash)
