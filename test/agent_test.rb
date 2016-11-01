2d0bba1dd23f9ad3a867dc9dff8906b05be5ceb7
Darren Jeacocke <dazonic@me.com>
2016-11-01 16:24:39 +1000

    Update URLs in tests

--- test/agent_test.rb
+++ test/agent_test.rb
@@ -9,43 +9,43 @@ class AgentTest < MiniTest::Test
 
   def test_get_url_2016_australia_practice1
     event = F1Results::Event.new(year: 2016, country: 'Australia', type: :practice1)
-    url = 'http://www.formula1.com/content/fom-website/en/results.html/2016/races/938/australia/practice-1.html'
+    url = 'http://www.formula1.com/en/results.html/2016/races/938/australia/practice-1.html'
     assert_equal url, @agent.get_url(event)
   end
 
   def test_get_url_2016_australia_qualifying
     event = F1Results::Event.new(year: 2016, country: 'Australia', type: :qualifying)
-    url = 'http://www.formula1.com/content/fom-website/en/results.html/2016/races/938/australia/qualifying.html'
+    url = 'http://www.formula1.com/en/results.html/2016/races/938/australia/qualifying.html'
     assert_equal url, @agent.get_url(event)
   end
 
   def test_get_url_2016_australia_race
     event = F1Results::Event.new(year: 2016, country: 'Australia', type: :race)
-    url = 'http://www.formula1.com/content/fom-website/en/results.html/2016/races/938/australia/race-result.html'
+    url = 'http://www.formula1.com/en/results.html/2016/races/938/australia/race-result.html'
     assert_equal url, @agent.get_url(event)
   end
 
   def test_get_url_1984_brazil_qualifying
     event = F1Results::Event.new(year: 1984, country: 'Brazil', type: :qualifying)
-    url = 'http://www.formula1.com/content/fom-website/en/results.html/1984/races/466/brazil/qualifying-0.html'
+    url = 'http://www.formula1.com/en/results.html/1984/races/466/brazil/qualifying-0.html'
     assert_equal url, @agent.get_url(event)
   end
 
   def test_get_url_1984_brazil_race
     event = F1Results::Event.new(year: 1984, country: 'Brazil', type: :race)
-    url = 'http://www.formula1.com/content/fom-website/en/results.html/1984/races/466/brazil/race-result.html'
+    url = 'http://www.formula1.com/en/results.html/1984/races/466/brazil/race-result.html'
     assert_equal url, @agent.get_url(event)
   end
 
   def test_get_url_1950_indianapolis_500_qualifying
     event = F1Results::Event.new(year: 1950, country: 'United States', type: :qualifying)
-    url = 'http://www.formula1.com/content/fom-website/en/results.html/1950/races/96/indianapolis-500/qualifying-0.html'
+    url = 'http://www.formula1.com/en/results.html/1950/races/96/indianapolis-500/qualifying-0.html'
     assert_equal url, @agent.get_url(event)
   end
 
   def test_get_url_1950_indianapolis_500_race
     event = F1Results::Event.new(year: 1950, country: 'United States', type: :race)
-    url = 'http://www.formula1.com/content/fom-website/en/results.html/1950/races/96/indianapolis-500/race-result.html'
+    url = 'http://www.formula1.com/en/results.html/1950/races/96/indianapolis-500/race-result.html'
     assert_equal url, @agent.get_url(event)
   end
 
@@ -67,7 +67,7 @@ class AgentTest < MiniTest::Test
     end
   end
 
-  def test_event_with_bad_grand_prix
+  def test_event_with_bad_country
     assert_raise_with_message RuntimeError, "No country 'New Zealand' found for 2016" do
       event = F1Results::Event.new(year: 2016, country: 'New Zealand', type: :race)
       @agent.get_url(event)
